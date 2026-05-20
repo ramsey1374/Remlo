ad028251087141f5779502de7ab3449a826e0bd7
Ramsey Agi <agi@Ramseys-Macbook-pro.local>
2026-05-20 00:38:59 -0400

    fix homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
-import CreateInvoicePage from "./create-invoice/page";
-
 export default function Home() {
-  return <CreateInvoicePage />;
+  return (
+    <main className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold">Remlo</h1>
+    </main>
+  );
 }
